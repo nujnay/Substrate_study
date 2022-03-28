be649db366aac0665053ab4c54f0f68ee6593315
Your Name <you@example.com>
2022-03-28 12:30:34 +0530

    1

---
+++ rust_study/variables/src/main.rs
@@ -0,0 +1,12 @@
+
+const MAX_POINT:u32 = 100_000;
+fn main() {
+    let x = 5;
+    const TT:i8 = 0;
+    let x = x + 1;
+    let x = x * 2;
+    let spaces = "   ";
+    let spaces = spaces.len();
+    print!("{}", spaces);
+    // let a = [3;5]
+}
