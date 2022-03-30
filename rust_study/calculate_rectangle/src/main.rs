22688d9d08d139f9dea62fdd9ef4dd71ff338d8a
Your Name <you@example.com>
2022-03-30 10:22:52 +0530

    1

---
+++ rust_study/calculate_rectangle/src/main.rs
@@ -0,0 +1,34 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    length: u32,
+
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.length
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.length > other.length
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            length: size,
+        }
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        length: 30,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        length: 10,
+    };
+    println!("{:#?}", rect.can_hold(&rect2));
+}
