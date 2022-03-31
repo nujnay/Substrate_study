a793c53d35f41b82e3a470a58bdccd8bd45e1ce3
yanjun <1292015395@qq.com>
2022-03-31 18:39:24 +0800

    20220331-183924

--- rust_study/collections_demo/src/main.rs
+++ rust_study/collections_demo/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
+    let mut v: Vec<i32> = Vec::new();
+    &v.push(1);
+    &v.push(2);
+    &v.push(3);
+    let third: &i32 = &v[2];
+    let first = &v[0];
+
+    let mut v2 = vec![1, 2, 3];
+    v2.push(2);
+
+
+    match v.get(100) {
+        Some(third) => println!("{}", third),
+        None => print!("no third element"),
+    }
+
+    for i in &mut v2 {
+        *i += 50;
+    }
+
+    for i in v2 {
+        println!("{}", i)
+    }
+
     println!("Hello, world!");
 }
