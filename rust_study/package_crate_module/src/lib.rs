e31241b1faaa2494c53386b16a0b1c08a0114657
yanjun <1292015395@qq.com>
2022-03-31 17:42:02 +0800

    20220331-174202

--- rust_study/package_crate_module/src/lib.rs
+++ rust_study/package_crate_module/src/lib.rs
@@ -1,5 +1,57 @@
-mod front_of_house{
-    mod hosting{
+mod front_of_house {
+    mod hosting {}
+}
 
+fn serve_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
+
+    pub struct BreatFast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl BreatFast {
+        pub fn summer(toast: &str) -> BreatFast {
+            BreatFast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+        crate::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    let mut meal = back_of_house::BreatFast::summer("RYE");
+    meal.toast = String::from("Wheat");
+    println!("{}", meal.toast);
+}
+
+use std::collections::HashMap;
+
+
+use std::fmt;
+use std::io::Result as IoResult;
+
+fn f1() -> fmt::Result() {
+    Result
+}
+fn f2() -> IoResult<i32> {
+    IoResult
+}
+fn main() {
+    let mut map = HashMap::new();
+    map.insert(1, 2);
 }
--- rust_study/package_crate_module/src/main.rs
+++ rust_study/package_crate_module/src/main.rs
@@ -1,3 +1,6 @@
+use package_crate_module::eat_at_restaurant;
+
 fn main() {
     println!("Hello, world!");
+    eat_at_restaurant();
 }
